264f8cdf1047dbcd5044fed9a86587df849a0fd3
Ingo Duarte <ingo.republica@gmail.com>
2018-10-22 16:47:14 -0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">
+    <h1>ring of truth</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.title {
+  padding: 20px;
+  margin-top: 20px;
+  border-bottom: 1px darkorange solid;
+  font-size: minmax(10px, 1.4vw);
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>  
+  <div class="postlist">
+    <div class="left"></div>
+    <div class="center">center</div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.postlist {
+  padding-top: 20px;
+  display: grid;
+  /* grid-template: "left center right"; */
+  grid-template-columns: minmax(1px, 1fr)
+                         minmax(300px, 800px)
+                         minmax(1px, 1fr);
+  color: rgb(224, 224, 224);
+  background-color: hsl(54, 9%, 12%);  
+}
+.center {
+  border: 1px black solid;
+}
+</style>
