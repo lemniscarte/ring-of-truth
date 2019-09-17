03dbd53bcc875fba6ec8653fbeb7afc2d43a7f57
Ingo Duarte <ingo.republica@gmail.com>
2019-09-16 20:35:05 -0700

    audit fix

--- src/components/Foldable.vue
+++ src/components/Foldable.vue
@@ -47,11 +47,6 @@ const defaultHeight = 120;
 
 export default {
   props: {
-    articleIsHere: {
-      type: Boolean,
-      default: false
-    },
-
     minHeight: {
       type: Number,
       default: defaultHeight
